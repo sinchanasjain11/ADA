603212a2ae4da1b19ed7dfeac444b95af69c3c01
Sinchana S <sinchanasjain11@gmail.com>
2026-03-13 11:27:01 +0530

    Add files via upload

--- topologicalsort2.py
+++ topologicalsort2.py
@@ -1,9 +1,8 @@
 def topological_sort(n, edges):
-    # Adjacency matrix
     adj = [[0]*n for _ in range(n)]
     indegree = [0]*n
 
-    # Build graph
+   
     for u, v in edges:
         adj[u][v] = 1
         indegree[v] += 1
@@ -11,22 +10,22 @@ def topological_sort(n, edges):
     topo_order = []
 
     for _ in range(n):
-        # Find a source vertex
+
         source = -1
         for i in range(n):
             if indegree[i] == 0:
                 source = i
                 break
 
-        # If no source found, cycle exists
+
         if source == -1:
             print("Topological sorting not possible (Graph has a cycle)")
             return
 
         topo_order.append(source)
-        indegree[source] = -1   # Mark as removed
+        indegree[source] = -1  
 
-        # Remove outgoing edges of source
+       
         for j in range(n):
             if adj[source][j] == 1:
                 indegree[j] -= 1
@@ -35,7 +34,7 @@ def topological_sort(n, edges):
     print(topo_order)
 
 
-# -------- Main --------
+
 n = int(input("Enter number of vertices: "))
 e = int(input("Enter number of edges: "))
 
@@ -46,3 +45,4 @@ for _ in range(e):
     edges.append((u, v))
 
 topological_sort(n, edges)
+
