2573a0b293ad39eeee44bb9332d7329e1a3ab265
Sinchana S <sinchanasjain11@gmail.com>
2026-03-19 14:54:55 +0530

    Update lab11.py

--- lab11.py
+++ lab11.py
@@ -1,3 +1,8 @@
+""" Design and implement C/JAVA/Python Program to sort a given set of n integer 
+elements using Merge Sort method and compute its time complexity. Run the 
+program for varied values of n> 5000, and record the time taken to sort. Plot a 
+graph of the time taken versus n. The elements can be read from a file or can be 
+generated using the random number generator. """
 import random
 import time
 
